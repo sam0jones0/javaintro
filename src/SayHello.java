a2276a43f237a0c81b6c6b4b9c48dfdb2e0f4327
Sam Jones <sam0jones0@gmail.com>
2022-09-09 16:30:13 +0100

    Added option to provide name and uploaded .class bytecode

--- src/SayHello.java
+++ src/SayHello.java
@@ -1,6 +1,11 @@
 public class SayHello {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        if (args.length > 0) {
+            System.out.println("Hello " + args[0] + ".");
+        }
+        else {
+            System.out.println("Hello");
+        }
     }
     public void Greeting () {
 
