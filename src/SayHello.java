c95aafc4b11d399fa121f219260ff60c123198cd
Sam Jones <sam0jones0@gmail.com>
2022-09-10 11:34:56 +0100

    Fix resource leak with line reader

--- src/SayHello.java
+++ src/SayHello.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class SayHello {
     public static void main(String[] args) {
-        Scanner reader = new Scanner(System.in);
-
-        System.out.println("Please enter your name:");
-        String name = reader.nextLine();
-        if (name != null) {
-            System.out.println("Hello " + name + ".");
-        } else {
-            System.out.println("Hello");
+        try (Scanner reader = new Scanner(System.in)) {
+            System.out.println("Please enter your name:");
+            final String name = reader.nextLine();
+            if (name != null) {
+                System.out.println("Hello " + name + ".");
+            } else {
+                System.out.println("Hello");
+            }
         }
     }
 
