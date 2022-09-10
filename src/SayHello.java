af9d58e5f9ed98a99ad11472fc7072f0784baa42
Sam Jones <sam0jones0@gmail.com>
2022-09-10 11:30:33 +0100

    Change to read input from stdin

--- src/SayHello.java
+++ src/SayHello.java
@@ -1,13 +1,19 @@
+import java.util.Scanner;
+
 public class SayHello {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            System.out.println("Hello " + args[0] + ".");
-        }
-        else {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Please enter your name:");
+        String name = reader.nextLine();
+        if (name != null) {
+            System.out.println("Hello " + name + ".");
+        } else {
             System.out.println("Hello");
         }
     }
-    public void Greeting () {
+
+    public void Greeting() {
 
     }
 }
