aaccd9c0b655067fcb92146b23ed0e9ae53b4512
Sam Jones <sam0jones0@gmail.com>
2022-09-11 13:44:47 +0100

    Refactor and Extend SayHello to count up to a number

--- src/SayHello.java
+++ src/SayHello.java
@@ -1,19 +1,40 @@
 import java.util.Scanner;
 
 public class SayHello {
-    public static void main(String[] args) {
-        try (Scanner reader = new Scanner(System.in)) {
-            System.out.println("Please enter your name:");
-            final String name = reader.nextLine();
-            if (name != null) {
-                System.out.println("Hello " + name + ".");
-            } else {
-                System.out.println("Hello");
-            }
+    static Scanner reader = new Scanner(System.in);
+
+    private static String readStdIn() {
+        while (reader.hasNextLine()) {
+            String input = reader.nextLine();
+            return input;
+        }
+        return "Error: Read empty line";
+    }
+
+    public static void greeting(String name) {
+        if (name != null) {
+            System.out.println("Hello " + name + ".");
+        } else {
+            System.out.println("Hello");
         }
     }
 
-    public void Greeting() {
+    public static void count(int countTo) {
+        for (int i = 1; i < countTo + 1; i++) {
+            System.out.println(i);
+        }
+    }
 
+    public static void main(String[] args) {
+        System.out.println("Please enter your name:");
+        final String name = readStdIn();
+        greeting(name);
+
+        System.out.println("Count to what number?");
+        final int countTo = Integer.parseInt(readStdIn());
+        count(countTo);
+
+        System.out.println("Goodbye.");
+        reader.close();
     }
 }
