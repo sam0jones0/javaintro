25a5f7be3a4eb142aef482bf60161816a1c6e642
Sam Jones <sam0jones0@gmail.com>
2022-09-12 16:44:43 +0100

    Add rest of months for conditionals switch exercise

--- src/Conditionals.java
+++ src/Conditionals.java
@@ -33,6 +33,21 @@ public class Conditionals {
         case 8: {
             System.out.println("Month number " + month + " is in Summer");
         }
+        case 9:
+        case 10:
+        case 11: {
+            System.out.println("Month number " + month + " is in Autumn");
+        }
+        case 12:
+        case 1:
+        case 2: {
+            System.out.println("Month number " + month + " is in Winter");
+        }
+        case 3:
+        case 4:
+        case 5: {
+            System.out.println("Month number " + month + " is in Spring");
+        }
         }
     }
 }
