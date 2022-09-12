30609beee4eea2638e67755dcef3639cd04297af
Sam Jones <sam0jones0@gmail.com>
2022-09-12 15:24:36 +0100

    Add Variables exercise code

--- src/VariablesExercise.java
+++ src/VariablesExercise.java
@@ -1,3 +1,38 @@
+import java.util.Scanner;
+
 public class VariablesExercise {
-    
+
+    static Scanner reader = new Scanner(System.in);
+
+    private static String readStdIn() {
+        while (reader.hasNextLine()) {
+            String input = reader.nextLine();
+            return input;
+        }
+        return "Error: Read empty line";
+    }
+
+    public static void main(String[] args) {
+        // String firstName = "Sam";
+        // String lastName = "Jones";
+        // String name = firstName + " " + lastName;
+        // System.out.println(name);
+
+        // int num1 = 1000000;
+        // int num2 = 1231;
+        // System.out.println(num1 + num2);
+
+        // float result = (float) num1 / num2;
+        // System.out.println(result);
+
+        // int num3 = 8;
+        // System.out.println(num3++);
+        // System.out.println(num3);
+        // System.out.println(++num3);
+
+        System.out.println("Please enter your first name:");
+        String fname = readStdIn();
+        System.out.println("And your second name?");
+        System.out.println("Hello " + fname + " " + readStdIn());
+    }
 }
