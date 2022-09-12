f74cbbfe040d9be2b1d2572f18d7d1dc3667a7d0
Sam Jones <sam0jones0@gmail.com>
2022-09-12 12:28:14 +0100

    Refactor folder structure.

--- src/Scratchpad.java
+++ src/Scratchpad.java
@@ -1,6 +1,7 @@
-
 public class Scratchpad {
     public static void main(String[] args) {
-        System.out.println("Goodbye.");
+        int var1 = 1;
+        var1++;
+        System.out.println(var1);
     }
 }
