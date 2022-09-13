d7386828a8ecc6a469674fbeafa7c6c4a63ef5c7
Sam Jones <sam0jones0@gmail.com>
2022-09-13 12:21:12 +0100

    Add student class from Classes exercise

---
+++ Student.java
@@ -0,0 +1,35 @@
+public class Student {
+    private byte age;
+    private String name;
+    private char grade;
+
+    public Student(byte age, String name, char grade) {
+        this.age = age;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public byte getAge() {
+        return this.age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getGrade() {
+        return grade;
+    }
+
+    public void setGrade(char grade) {
+        this.grade = grade;
+    }
+}
