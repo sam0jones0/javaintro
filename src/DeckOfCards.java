710f2c12602417c9f9c9310aefc5825e09bce8cf
Sam Jones <sam0jones0@gmail.com>
2022-09-13 15:33:21 +0100

    Add Deck of cards

---
+++ src/DeckOfCards.java
@@ -0,0 +1,48 @@
+public class DeckOfCards {
+
+    private Card[] deck;
+
+    public class Card {
+        private char suit;
+        private String value;
+
+        public Card(char suit, String value) {
+            this.suit = suit;
+            this.value = value;
+        }
+
+        public char getSuit() {
+            return suit;
+        }
+
+        public void setSuit(char suit) {
+            this.suit = suit;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+
+    }
+
+    public DeckOfCards() {
+        Card[] deck = new Card[52];
+
+        char[] suits = { 'C', 'H', 'S', 'D' };
+        String[] values = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", 
+                            "J", "Q", "K" };
+
+        int pos = 0;
+        for (String value : values) {
+            for (char suit : suits) {
+                deck[pos] = new Card(suit, value);
+                pos++;
+            }
+
+        }
+    }
+}
