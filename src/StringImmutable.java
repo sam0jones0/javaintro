f5b06ed70ea342949531dd65e40fc78fab4906ac
Sam Jones <sam0jones0@gmail.com>
2022-09-13 16:26:21 +0100

    Add String Immutable exercise

---
+++ src/StringImmutable.java
@@ -0,0 +1,11 @@
+public class StringImmutable {
+    public static void main(String[] args) {
+        String s = "Hello";
+        meth(s);
+        System.out.println(s);
+    }
+
+    public static void meth(String x) {
+        x = x + " World!";
+    }
+}
