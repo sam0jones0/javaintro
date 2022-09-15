1838ce1ce9e678f4bdb50cb026e7a4ee9f68eee0
Sam Jones <sam0jones0@gmail.com>
2022-09-15 14:50:12 +0100

    Overload varargs printer to accept String too

--- com/qa/VarargsExample/VarArgsPrinter.java
+++ com/qa/VarargsExample/VarArgsPrinter.java
@@ -10,4 +10,14 @@ public class VarArgsPrinter {
             }
         }
     }
+
+    VarArgsPrinter(String... args) {
+        for (int i = 0; i < args.length - 1; i++) {
+            System.out.print(args[i] + ", ");
+            if (i == args.length - 2) {
+                System.out.println(args[args.length - 1]);
+                break;
+            }
+        }
+    }
 }
--- com/qa/VarargsExample/VarargsController.java
+++ com/qa/VarargsExample/VarargsController.java
@@ -3,10 +3,17 @@ package com.qa.VarargsExample;
 public class VarargsController {
     public static void main(String[] args) {
         printArgs(1, 2, 3, 4, 5);
+        printArgs("one", "two", "three");
     }
 
     public static void printArgs(int... args) {
         VarArgsPrinter vargs = new VarArgsPrinter(args);
         System.out.println("Number of Arguments: " + args.length);
     }
+
+    public static void printArgs(String... args) {
+        VarArgsPrinter vargs = new VarArgsPrinter(args);
+        System.out.println("Number of Arguments: " + args.length);
+    }
+
 }
